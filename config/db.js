ed2527f3a6aba22933ace297157943ad2c2f6ed5
MD Iktedar Hasan Rushdi Rajin <rushdi2410@gmail.com>
2024-11-14 23:58:47 +0600

    fixed some connection in cofing

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const MONGO_URI = process.env.MONGO_URI;
 const connectDB = async () => {
   try {
     await mongoose
-      .connect(MONGO_URI)
+      .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
       .then(() => console.log("Connected to MongoDB Atlas"))
       .catch((error) => console.error("MongoDB connection error:", error));
   } catch (error) {
