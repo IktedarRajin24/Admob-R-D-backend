cbab68551287d40102598d835e09a99353a77bdc
MD Iktedar Hasan Rushdi Rajin <rushdi2410@gmail.com>
2024-12-08 03:38:39 +0600

    made some adjustments (see comments)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { signup, login, getAllUsers } from "../controllers/userController.js";
-import { authenticateToken } from "../middlewares/auth.js";
+import { signup, login } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", signup);
 userRouter.post("/login", login);
-userRouter.get("/users", authenticateToken, getAllUsers);
+// userRouter.get("/users", authenticateToken, getAllUsers);
 
 export default userRouter;
